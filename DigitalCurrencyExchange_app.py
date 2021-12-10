552c8b78293e672949001f92a966458d44518878
jskshan <62166930+jskshan@users.noreply.github.com>
2021-12-10 17:19:31 +0800

    Add files via upload

--- DigitalCurrencyExchange_app.py
+++ DigitalCurrencyExchange_app.py
@@ -6,60 +6,61 @@ import numpy as np
 import pandas as pd
 
 st.set_page_config(
-    page_title="Digital Currency Exchange",
+    page_title="Digital Currency(USDT) Exchange",
     page_icon="random",
     layout="centered",
     initial_sidebar_state="collapsed",
 )
 
-st.title("Digital Currency Exchange")
+dIuputUSDT = 0.0
+
+st.title("Digital Currency(USDT) Exchange")
 
 sExchangePlatform = st.selectbox("Exchange Platform", ("Pionex", "ACE"))
-st.write('You selected:', sExchangePlatform)
+st.write("Your Selecte : ", sExchangePlatform)
 
 nInputTWD = st.text_input("TWD Dollar", "100000")
-st.write("TWD Dollar : ", nInputTWD)
+nInputTWD = int(nInputTWD)
+st.write("TWD Dollar : {0}".format(nInputTWD))
 
 dInputExchangeRate = st.text_input("{0} Exchange Rate".format(sExchangePlatform), "27.942")
-st.write("{0} Exchange Rate : ".format(sExchangePlatform), dInputExchangeRate)
+dInputExchangeRate = round(float(dInputExchangeRate), 3)
+
+if sExchangePlatform == "ACE":
+    dInputFee = 0.1
+    
+    dIuputUSDT = (nInputTWD / dInputExchangeRate) * (1 - (dInputFee / 100))
+elif sExchangePlatform == "Pionex":
+    nExchangeCount = 100000 // 30000
+        
+    if (100000 % 30000) > 0:
+        nExchangeCount = nExchangeCount + 1
+    
+    dInputFee = nExchangeCount * 20
+    nOutputFee = 0.1
+    
+    dIuputUSDT = (nInputTWD - dInputFee) / dInputExchangeRate
+
+st.write("{0} Exchange Rate : {1}, USDT Amount : {2}".format(sExchangePlatform, dInputExchangeRate, dIuputUSDT))
 
 dOutputExchangeRate = st.text_input("MAX Exchange Rate", "28.049")
-st.write("MAX Exchange Rate : ", dOutputExchangeRate)
+dOutputExchangeRate = round(float(dOutputExchangeRate), 3)
+
+if sExchangePlatform == "ACE":
+    nSendUSDTFee = 0
+    dOutputFee = 0.1
+elif sExchangePlatform == "Pionex":
+    nSendUSDTFee = 1
+    dOutputFee = 0.1
+
+dRecieveUSDT = dIuputUSDT - nSendUSDTFee
+
+st.write("Recieve USDT : {0}, MAX Exchange Rate : {1}".format(dRecieveUSDT, dOutputExchangeRate))
 
 if st.button("Compute"):
-    nInputExchangeFee = 0 #15
-    nOutputExchangeFee = 15
-    
-    nSendUSDTFee = 1
-    
-    nInputTWD = int(nInputTWD)
-    dInputExchangeRate = float(dInputExchangeRate)
-    dOutputExchangeRate = float(dOutputExchangeRate)
-    
-    if sExchangePlatform == "ACE":
-        nSendUSDTFee = 0
-        nInputFee = 0.1
-        nOutputFee = 0.1
+    dOutputTWD = (dRecieveUSDT * dOutputExchangeRate) * (1 - (dOutputFee / 100))
+    dRewardRate = round(((dOutputTWD - nInputTWD) / nInputTWD) * 100, 2)
         
-        dIuputUSDT = (nInputTWD / dInputExchangeRate) * (1 - (nInputFee / 100))
-        dSendUSDT = dIuputUSDT - nSendUSDTFee
-        
-        dOutputTWD = (dSendUSDT * dOutputExchangeRate) * (1 - (nOutputFee / 100)) - nInputExchangeFee - nOutputExchangeFee
-        dRewardRate = round(((dOutputTWD - nInputTWD) / nInputTWD) * 100, 2)
-    elif sExchangePlatform == "Pionex":
-        nExchangeCount = 100000 // 30000
-        
-        if (100000 % 30000) > 0:
-            nExchangeCount = nExchangeCount + 1
-        
-        nInputFee = nExchangeCount * 20
-        nOutputFee = 0.1
-        
-        dIuputUSDT = (nInputTWD - nInputFee) / dInputExchangeRate
-        dSendUSDT = dIuputUSDT - nSendUSDTFee
-        
-        dOutputTWD = (dSendUSDT * dOutputExchangeRate) * (1 - (nOutputFee / 100)) - nInputExchangeFee - nOutputExchangeFee
-        dRewardRate = round(((dOutputTWD - nInputTWD) / nInputTWD) * 100, 2)
     st.write("Result:")
     st.write("Output TWD Dollar : {0}".format(dOutputTWD))
     st.write("Reward Rate = {0}%".format(dRewardRate))
